26be1977121bc5af937d04e56c10449eb2f4b89e
nihalraza369 <nihalraza369@gmail.com>
2025-01-05 02:17:23 +0500

    initialize repo

---
+++ js noni/noni.js
@@ -0,0 +1,96 @@
+
+var names = "nihal"
+var names ="noman"
+  var names ="bilal"
+  var names = "rafay"
+  console.log(names)
+{
+let a = 213;
+console.log(a)
+}
+{
+let a = 453;
+console.log(a);
+
+}
+
+let a = 2345;
+
+
+const b = "nughman hussain"
+
+
+console.log(a);
+console.log(b);
+
+
+let student ={
+    name:803030,
+    age :16,
+    fname:"nughman hussain",
+    rollnumber:34567
+}
+console.log(student)
+
+
+
+//Operator;
+//l = 2
+//n = 3
+// console.log("l + n =",l+n);
+// console.log(l-n);
+// console.log(l*n);
+// console.log(l/n)
+// console.log(l%n)
+// console.log(l**n);
+ 
+
+//console.log("l =",l--) 
+ 
+
+
+
+
+
+//Assignment operator
+
+let m = 4
+let k = 6
+ m **=2
+console.log(m)
+
+
+//comperision operator;
+
+//let l = "2"
+//let n = 2
+//console.log(l==n);
+//console.log(l!=n);
+//console.log(l===n);
+//console.log(l!==n);
+
+
+
+//Logical operator;
+
+
+
+
+c =5
+d =5
+cond=c<6
+cond1=c===d
+console.log("cond & cond1 =",cond  cond );
+
+
+
+
+
+
+
+
+
+
+
+
+
